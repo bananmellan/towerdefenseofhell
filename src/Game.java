6caa07bff90ae21784746aef4e1f10ae2688e11f
Saft23 <joel.ardarve@gmail.com>
2019-06-27 19:39:29 +0200

    Added initial code to get a board up and running

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,9 @@
 public class Game{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Running main");
+        Board board = new Board();
+        while(true){
+        }
+
     }
 }
